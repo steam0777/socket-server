435bb905843efbd5e72168f907bfd3c2d13ecff2
steam0777 <steamluk07@gmail.com>
2020-01-02 16:21:43 -0400

    Servidor escuchando mensajes de angular basico

---
+++ classes/server.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import { SERVER_PORT } from '../global/environment';
+import http from 'http';
+import socketIO from 'socket.io';
+
+import * as socket from '../sockets/socket';
+
+export default class Server{
+
+    private static _intance: Server;
+
+    public app: express.Application;
+    public port: number;
+
+    public io: SocketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor(){
+
+        this.app = express();
+        this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
+    }
+
+    public static get instance(){
+
+        return this._intance || ( this._intance = new this() );
+
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente =>{
+            console.log('Cliente conectado');
+
+            //  MENSAJES
+            socket.mensaje( cliente, this.io );
+
+            //  DESCONECTAR
+            socket.desconectar( cliente );
+        });
+    }
+
+    start( callback: any ){
+
+        this.httpServer.listen( this.port, callback );  
+
+    }
+
+}
---
+++ sockets/README.md
@@ -0,0 +1,16 @@
+# Servidor Socket
+Reconstruir modulos de NODE
+```````
+npm install
+```````
+
+Generar el dist/
+```````
+tsc -w
+```````
+
+Levantar el servidor, cualquier de los comandos
+```````
+nodemon dist/
+node dist/
+```````
